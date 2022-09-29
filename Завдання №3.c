cbe4000a7d0cf8eb728f3edff01fc514d6be06d6
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2022-09-29 12:09:27 +0300

    Лаба№2

---
+++ Завдання №3.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() { //Третє завдання
+
+	int a, y, z, x;
+	printf_s("Enter a:\n");
+	scanf_s("%d", &a);
+	printf_s("Enter y:\n");
+	scanf_s("%d", &y);
+	printf_s("Enter z:\n");
+	scanf_s("%d", &z);
+
+	x = a * sqrt(pow(y, 2) - (2 * a)) + pow(cos(z), 2);
+
+	printf("Respond: x = %d\n", x);
+
+
+	return 0;
+}
