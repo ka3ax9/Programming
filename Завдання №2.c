90741144455f900de16e54d02e3431fe5783336d
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2022-11-23 20:25:59 +0200

    Update Завдання №2.c

--- Завдання №2.c
+++ Завдання №2.c
@@ -1,25 +1,17 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <math.h>
 
 int main() { // Друге завдання
-
-	float a, b, c;
-	printf_s("Enter a:\n");
-	scanf_s("%f", &a);
-	printf_s("Enter b:\n");
-	scanf_s("%f", &b);
-	printf_s("Enter c:\n");
-	scanf_s("%f", &c);
+	const int a = 15, b = -2, c = 3;
 	float y;
+
+	printf("Given: y = (((3 * a) - (4 * b) + (5 * c)) / ((a * b) + (2 * b * c) + (3 * a * c))) - (((a * b) - c) / 4)\n");
+	printf("Constant values: a = 15, b = -2, c = 3 :\n");
+
 
 	y = (((3 * a) - (4 * b) + (5 * c)) / ((a * b) + (2 * b * c) + (3 * a * c))) - (((a * b) - c) / 4);
 
-	printf_s("Respond: = %.1f", y);
-
-
-
-
-
+	printf("Respond: = %.1f", y);
 
 
 
